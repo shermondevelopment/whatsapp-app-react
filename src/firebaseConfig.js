a16d3b1b64e36e2498d6350dbd83d4eb39000c22
Victor Shermon <66441063+shermondevelopment@users.noreply.github.com>
2021-08-30 11:53:45 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,6 @@
 export default  {
     apiKey: "AIzaSyCyR2e_9COg0p-HQOkSz8AjVa3traISFPA",
-    authDomain: "whatsappclone-27b28.firebaseapp.com",
+    authDomain: "https://clone-whatsapp-reactjs.herokuapp.com/",
     databaseURL: "https://whatsappclone-27b28.firebaseio.com",
     projectId: "whatsappclone-27b28",
     storageBucket: "whatsappclone-27b28.appspot.com",
